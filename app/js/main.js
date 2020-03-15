4471ed0a45e2d52af1cd1c232462031a461a050c
barovskiy777 <barovskiyboris@gmail.com>
2020-03-15 15:29:02 +0300

    layout until team section

---
+++ app/js/main.js
@@ -0,0 +1,31 @@
+$(function () {
+
+    $('button[filter]').click(function (e) {
+        e.preventDefault();
+        let $this = $(this),
+            works = $('.filter__item'),
+            thisClass = $this.attr('filter'),
+            divShow = $('.' + thisClass),
+            allBtn = $('button[filter]');
+
+
+        if ($this.hasClass('active')) console.log(1);
+        else if (thisClass == 'all') {
+            works.fadeOut(0);
+            works.fadeIn(600);
+            allBtn.removeClass('active');
+            $this.addClass('active');
+            console.log(1);
+        } else {
+            console.log(2);
+            works.fadeOut(0)
+            divShow.fadeIn(600);
+            allBtn.removeClass('active');
+            $this.addClass('active');
+        }
+
+    });
+
+
+
+});
